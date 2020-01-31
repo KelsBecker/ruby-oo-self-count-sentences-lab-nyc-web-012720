0be1098f4503df2672fac148e15ae3bcb6bfbf1b
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-01-31 15:49:23 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+      string_array = self.split
+      count = 0
 
-  end
+      if string_array.each do |word|
+        last_element = word.length - 1
+        if word[last_element] == "." or
+           word[last_element] == "!" or 
+           word[last_element] == "?"
+           count += 1
+        end
+       end
+      return count
+     end
+    end
 end
